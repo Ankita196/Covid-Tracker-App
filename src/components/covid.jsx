018a22002ec744e9e52e64fad5be5d62670dfa89
Ankita196 <muskanmakde1999@gmail.com>
2021-09-13 18:07:20 +0530

    1 file modified

--- src/components/covid.jsx
+++ src/components/covid.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 
 
 export default function Covid() {
+ const [data, setData]= useState([])
+const getCovidData= async ()=>{
+try {
+const res= await fetch('https://data.covid19india.org/data.json');
+const actualData= await res.json()
+console.log(actualData.statewise[0])
+setData(actualData.statewise[0])
+}catch(err){
+  console.log(err)
+}
+}
+
+useEffect(()=>{
+// getCovidData()
+},[])
+
   return (
     <div>
       <h1>Covid</h1>
-      <p></p>
+      <p>{data.active}</p>
     </div>
   );
 }

9819dad4cbcdb32805d175950d50129ccca13c0c
Ankita196 <muskanmakde1999@gmail.com>
2021-09-13 18:07:59 +0530

    1 file modified

--- src/components/covid.jsx
+++ src/components/covid.jsx
@@ -1,4 +1,25 @@
 import React, {useEffect,useState} from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
 
 export default function Covid() {
